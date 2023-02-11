b2ccd0571264e254f1b8273caeb1a291a62a7deb
Bruno Marcondes <brunomeneguim@hotmail.com>
2023-02-11 04:30:34 -0300

    ...

--- src/paginas/PaginaInicial/SecaoGeral/SecaoInstagram/index.js
+++ src/paginas/PaginaInicial/SecaoGeral/SecaoInstagram/index.js
@@ -8,7 +8,7 @@ export default function SecaoInstagram() {
             <Link className='link-instagram' to='https://www.instagram.com/casadanoiva.gpuava' target="_blank">Redes Sociais</Link>
             <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
             <iframe src="https://cdn.lightwidget.com/widgets/deb224217e0351188bd81a5a81a8c92e.html" 
-            title='Instagram'
+            title='Instagram Frame'
             scrolling="no" 
             allowtransparency="true" 
             className="lightwidget-widget"></iframe>
