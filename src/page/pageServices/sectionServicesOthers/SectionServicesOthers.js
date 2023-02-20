d88146bb2f484c6873a5d8697d0442cfbebf5232
Bruno Marcondes <brunomeneguim@hotmail.com>
2023-02-20 03:03:33 -0300

    .

--- src/page/pageServices/sectionServicesOthers/SectionServicesOthers.js
+++ src/page/pageServices/sectionServicesOthers/SectionServicesOthers.js
@@ -6,10 +6,10 @@ import styles from "./SectionServicesOthers.module.css";
 
 export default function SectionServicesOthers() {
     return (
-        <section className={styles.sectionServicesOthers} id="others" key={OthersData.id}> {
+        <section className={styles.sectionServicesOthers} id="others" > {
             OthersData && OthersData.map(OthersData => {
                 return (
-                    <div className={styles.cntServicesOthers}>
+                    <div className={styles.cntServicesOthers} key={OthersData.id}>
                         <div className={styles.boxServicesOthers}>
                             <h1 className={styles.titleServicesOthers}>{OthersData.titleOthers}</h1>
                             <p className={styles.textServicesOthers}>{OthersData.textOthers}</p>
