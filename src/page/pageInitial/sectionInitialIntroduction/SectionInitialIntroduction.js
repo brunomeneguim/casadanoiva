699178c2ab9076deb241e04a49e2fde05a88afb6
Bruno Marcondes <brunomeneguim@hotmail.com>
2023-03-08 00:09:23 -0300

    .

--- src/page/pageInitial/sectionInitialIntroduction/SectionInitialIntroduction.js
+++ src/page/pageInitial/sectionInitialIntroduction/SectionInitialIntroduction.js
@@ -1,24 +1,22 @@
 /* Desenvolvido por - Bruno Marcondes */
 /* Função que exporta a Seção Introdução para a página Inicial */
 
-import TextIntro from './textInitialIntro/TextInitialIntro';
+import TextIntro from "./textInitialIntro/TextInitialIntro";
 
-import styles from './SectionInitialIntroduction.module.css';
-
+import styles from "./SectionInitialIntroduction.module.css";
 
 export default function SectionInitialIntroduction() {
-    return (
-
-        <section className={styles.secaoInicialIntroducao}>
-            <TextIntro
-                titleIntro="ATELIÊ CASA DA NOIVA"
-                subtitleIntro="Aluguel de vestidos de noiva e acessórios"
-                textIntro="Escolher o vestido de noiva ideal é um dos momentos
+  return (
+    <section className={styles.secaoInicialIntroducao}>
+      <TextIntro
+        titleIntro="ATELIÊ CASA DA NOIVA"
+        subtitleIntro="Locação de vestidos de noiva e acessórios"
+        textIntro="Escolher o vestido de noiva ideal é um dos momentos
                 mais marcantes na preparação para o grande dia. Por isso a Casa da Noiva oferece
                 uma experiência única na procura do seu vestido e acessórios, com atendimento
                 personalizado em horário marcado, modelos incríveis e com valores de locação mais
                 acessíveis. Tudo pensando em te ajudar a ser a noiva mais linda em seu casamento."
-            />
-        </section>
-    );
+      />
+    </section>
+  );
 }
